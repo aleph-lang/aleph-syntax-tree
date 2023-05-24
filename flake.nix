cc1c3bfe48aeafb415bb9fd41bd69ddfd42d5209
jsz4n <3914641+jsz4n@users.noreply.github.com>
2023-05-25 00:50:04 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default (import rust-overlay)];
+        };
+
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.61.0";
+          rustChannel = "stable";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (rustVersion.override { extensions = [ "rust-src" ]; }) # for rust
+            cargo-nextest
+            pkg-config
+          ];
+        };
+    });
+}
